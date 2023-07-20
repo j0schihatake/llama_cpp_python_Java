09c503d48c1511b9a1a863d2543d80494a8325c9
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-20 13:45:37 +0300

    пробное решение.

--- src/main/java/com/j0schi/llama_cpp_python_java/utils/PythonExecutor.java
+++ src/main/java/com/j0schi/llama_cpp_python_java/utils/PythonExecutor.java
@@ -1,10 +1,33 @@
 package com.j0schi.llama_cpp_python_java.utils;
 
+import jakarta.annotation.PostConstruct;
 import org.python.util.PythonInterpreter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PythonExecutor {
+
+    private Llama llm;
+
+    @PostConstruct
+    public void llamaInit(){
+        String modelName = "vicuna-7b-v1.3-superhot-8k.ggmlv3.q5_K_M.bin";
+        PythonInterpreter interpreter = new PythonInterpreter();
+
+        // Запустите код Python из main.py
+        String pythonCode =
+                "from llama_cpp import Llama\n" +
+                "print('Loading model...')\n" +
+                "llm = Llama(model_path='/home/llama-cpp-user/model/" + modelName + "')\n" +
+                "print('Model loaded!')\n";
+
+        interpreter.exec(pythonCode);
+
+        // Получите результат выполнения кода Python
+        String result = interpreter.get("result").toString();
+
+        interpreter.close();
+    }
 
     public String executePythonCode(String question) {
         PythonInterpreter interpreter = new PythonInterpreter();
