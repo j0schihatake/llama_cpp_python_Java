5683eaf01e46afa9cfabe1302b78d68a2861ea44
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-21 01:34:35 +0300

    Почистил и добавил примеры запросов.

--- src/main/java/com/j0schi/llama_cpp_python_java/config/BeanConfig.java
+++ src/main/java/com/j0schi/llama_cpp_python_java/config/BeanConfig.java
@@ -1,18 +1,12 @@
 package com.j0schi.llama_cpp_python_java.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.j0schi.llama_cpp_python_java.utils.PythonExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BeanConfig {
-
-    @Bean
-    public PythonExecutor pythonExecutor(){
-        return new PythonExecutor();
-    }
 
     @Bean
     public RestTemplate restTemplate(){
