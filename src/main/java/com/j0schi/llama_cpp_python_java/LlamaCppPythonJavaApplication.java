96bdc5bfb5b2daea901c40e8a7896deac12cf561
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-20 11:28:17 +0300

    Добавил проект.

---
+++ src/main/java/com/j0schi/llama_cpp_python_java/LlamaCppPythonJavaApplication.java
@@ -0,0 +1,13 @@
+package com.j0schi.llama_cpp_python_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamaCppPythonJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlamaCppPythonJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j0schi/llama_cpp_python_java/LlamaCppPythonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0schi.llama_cpp_python_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlamaCppPythonJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
