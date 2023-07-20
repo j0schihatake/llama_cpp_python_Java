d1825cdb80c1b42defaba37b4b69fd0496682d1a
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-20 11:41:57 +0300

    Добавил первоначальные файлы.

---
+++ src/main/java/com/j0schi/llama_cpp_python_java/model/MessageRequest.java
@@ -0,0 +1,13 @@
+package com.j0schi.llama_cpp_python_java.model;
+
+public class MessageRequest {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
