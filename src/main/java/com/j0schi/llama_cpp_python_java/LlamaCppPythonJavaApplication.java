58f6d724cf1e113259e2e53bfc9e8677b7156d15
j0schiTadamori <j0schitadamori@gmail.com>
2023-07-20 14:21:21 +0300

    Меняю порт в коде.

--- src/main/java/com/j0schi/llama_cpp_python_java/LlamaCppPythonJavaApplication.java
+++ src/main/java/com/j0schi/llama_cpp_python_java/LlamaCppPythonJavaApplication.java
@@ -3,10 +3,16 @@ package com.j0schi.llama_cpp_python_java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class LlamaCppPythonJavaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LlamaCppPythonJavaApplication.class, args);
+        SpringApplication app = new SpringApplication(LlamaCppPythonJavaApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
     }
+
 }
